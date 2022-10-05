a5f981ebb05b9d75b12da883be3543c58f931927
Ramazan <29.rbulbul@gmail.com>
2022-10-05 11:00:49 +0200

    Edited DbConnection.cs

--- SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -4,6 +4,7 @@ using MongoDB.Driver;
 namespace SuggestionAppLibrary.DataAccess;
 public class DbConnection
 {
+   // These are set of connection to our database and we have called that "devsuggestionapp". It will create for us to first time we connect
    private readonly IConfiguration _config;
    private readonly IMongoDatabase _db;
    private string _connectionId = "MongoDB";
@@ -12,6 +13,7 @@ public class DbConnection
    public string StatusCollectionName { get; private set; } = "statuses";
    public string UserCollectionName { get; private set; } = "users";
    public string SuggestionCollectionName { get; private set; } = "suggestions";
+   // We need to connect to our collection outside of this db connection
    public MongoClient Client { get; private set; }
    public IMongoCollection<CategoryModel> CategoryCollection { get;private set; }
    public IMongoCollection<StatusModel> StatusCollection { get;private set; }
@@ -22,5 +24,16 @@ public class DbConnection
    public DbConnection(IConfiguration config)
    {
       _config = config;
+      // Created new client
+      Client = new MongoClient(_config.GetConnectionString(_connectionId));
+      // Connection to our database -> grab databasename -> connect to that db
+      DbName = _config[key: "DatabaseName"];
+      _db = Client.GetDatabase(DbName);
+
+      // connection to all four our collection
+      CategoryCollection = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+      StatusCollection = _db.GetCollection<StatusModel>(StatusCollectionName);
+      UserCollection = _db.GetCollection<UserModel>(UserCollectionName);
+      SuggestionCollection = _db.GetCollection<SuggestionModel>(SuggestionCollectionName);
    }
 }
